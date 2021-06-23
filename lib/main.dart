e160f93d47597140ee4229856e04216e243354d9
maheshwaran-p <mpmahesh1022@gmail.com>
2021-06-23 15:19:35 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: MyHomePage(title: 'Flutter Animation Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  bool _isPlaying = false;
+  AnimationController _animationController;
+  Animation<double> _pulseAnimation;
+
+  void animate() {
+    if (_isPlaying) {
+      _animationController.stop();
+    } else {
+      _animationController.forward();
+    }
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+
+    _pulseAnimation = Tween<double>(begin: 0.0, end: 5.6).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeIn));
+
+    _pulseAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _animationController.forward();
+        setState(() {
+          _animationController.stop();
+          _isPlaying = !_isPlaying;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+//      appBar: AppBar(
+//        title: Text(widget.title),
+//      ),
+      body: Center(
+        child: Card(
+          color: Colors.transparent,
+          elevation: 4,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          margin: EdgeInsets.all(16.0),
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ScaleTransition(
+                  scale: _pulseAnimation,
+                  child: Container(
+                    // color: Colors.white,
+                    height: 500,
+                    width: 380,
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text('',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black, fontSize: 24, letterSpacing: 2)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: animate,
+        tooltip: 'Increment',
+        child: _isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
